96f14602b22de80403aed24f204e838d37bcae8e
UNDaniel <daniel@abacus.co>
2017-09-19 22:25:21 +1000

    no message

--- UNDanielBasicTool_Swift/Classes/Helpers/UNDCurrencyFormatRelatedHelper.swift
+++ UNDanielBasicTool_Swift/Classes/Helpers/UNDCurrencyFormatRelatedHelper.swift
@@ -8,6 +8,7 @@
 
 class UNDCurrencyFormatRelatedHelper: UNDBaseModel {
     
+    //// MARK : - Properties
     public var usingLocaleIdentifier : String?
     
     public var usingCurrencyNumberFormatter : NumberFormatter{
@@ -19,6 +20,8 @@ class UNDCurrencyFormatRelatedHelper: UNDBaseModel {
         return returnFormatter
     }
     
+    
+    //// MARK : Private
     private var usingLocale : Locale{
         var returnLocale : Locale?
         if let unwrapedLocaleIdentifier = usingLocaleIdentifier {
@@ -26,5 +29,18 @@ class UNDCurrencyFormatRelatedHelper: UNDBaseModel {
         }
         return returnLocale ?? Locale.current
     }
+    
+    //// MARK : - General Methods
+    public func logCurrentLocaleInformation() -> String{
+        let checkingLocale = usingLocale
+        let checkingCurrencyNumberFormatter = usingCurrencyNumberFormatter
+        let logMessage = "Locale Identifier : \(checkingLocale.identifier) \n Locale Language Code : \(String(describing: checkingLocale.languageCode)) \n Currency Symbol : \(String(describing: checkingLocale.currencySymbol)) \n Currency Code : \(String(describing: checkingLocale.currencyCode)) \n Grouping Seperator : \(String(describing: checkingLocale.groupingSeparator)) \n Decimal Seperator : \(String(describing: checkingLocale.decimalSeparator)) \n Maximu Fraction Digits : \(checkingCurrencyNumberFormatter.maximumFractionDigits) \n Maximum Integer Digits : \(checkingCurrencyNumberFormatter.maximumIntegerDigits) \n Group Size : \(checkingCurrencyNumberFormatter.groupingSize)"
+        self.performConsoleLog(logMessage);
+        return logMessage
+    }
 
+    //// If the currency value you passed in is empty, we will treat it as 0.
+    public func formatCurrencyStringFromValue(_ currencyValue: NSNumber?) -> String {
+        return usingCurrencyNumberFormatter.string(from: currencyValue ?? NSNumber.init(value: 0))!
+    }
 }
